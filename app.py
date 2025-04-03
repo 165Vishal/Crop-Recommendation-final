55ccd8c265c2e6e9ab70890922cb67e410fc367b
vishal <vishalvvh1@gmail.com>
2025-04-03 13:52:38 +0530

    Added Procfile

--- app.py
+++ app.py
@@ -176,5 +176,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
